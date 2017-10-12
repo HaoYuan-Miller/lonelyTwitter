431518185cefa0b2d4df8ae3a0fc8563d98ae239
HaoYuan-Miller <hyuan2@ualberta.ca>
2017-10-11 18:57:30 -0600

    finished lab6

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 public class TweetList {
     private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 
-    public TweetList(){
-
-    }
+    public TweetList(){}
 
     public Tweet getTweet(int index){
         return tweets.get(index);
@@ -17,8 +15,20 @@ public class TweetList {
         return tweets.contains(tweet);
     }
 
-    public void add(Tweet tweet) {
-        tweets.add(tweet);
+    public void add(Tweet tweet) throws IllegalArgumentException {
+        if(this.hasTweet(tweet)) {
+            throw new IllegalArgumentException();}
+        else {
+            tweets.add(tweet);
+        }
+    }
+
+    public ArrayList<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public int getCount(){
+        return tweets.size();
     }
 
     public void delete(Tweet tweet) {
